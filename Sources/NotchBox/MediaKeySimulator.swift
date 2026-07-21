40dfcdba8d721c04f8db21ccf1b12cff260048f7
Daniel Gidrewicz <daniel.hidrevich9@gmail.com>
2026-07-21 18:22:02 +0200

    Fixed back button

--- Sources/NotchBox/MediaKeySimulator.swift
+++ Sources/NotchBox/MediaKeySimulator.swift
@@ -15,7 +15,7 @@ struct MediaKeySimulator {
         case .next:
             js = "document.querySelector('[data-testid=\"control-button-skip-forward\"]')?.click(); 'ok'"
         case .previous:
-            js = "document.querySelector('[data-testid=\"control-button-skip-backward\"]')?.click(); 'ok'"
+            js = "document.querySelector('[data-testid=\"control-button-skip-back\"], .control-button.spoticon-skip-back-16')?.click(); 'ok'"
         }
         _ = TrackInfoFetcher.runJSOnSpotifyTab(js)
     }
